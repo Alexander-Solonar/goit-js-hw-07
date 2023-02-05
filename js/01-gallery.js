7859e1b8885c52bccd5a301fd9d71f16968c7c23
Alexander-Solonar <112549802+Alexander-Solonar@users.noreply.github.com>
2023-02-05 22:01:59 +0200

    start task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const cardItem = creategalleryCardItem(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.insertAdjacentHTML("afterbegin", cardItem);
+galleryContainer.addEventListener("click", onGalleryComtainerClick);
+
+function creategalleryCardItem(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href= "${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onGalleryComtainerClick(event) {
+  event.preventDefault();
+  console.log(event.target.dataset.source);
+}
